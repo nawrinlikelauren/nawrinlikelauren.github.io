f90552ef1aa91a63d431b7112da5fdf31400072f
nawrinlikelauren <107654341+nawrinlikelauren@users.noreply.github.com>
2025-04-19 20:29:51 -0400

    Delete https: directory

--- https:/github.com/website/js/animations.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const header = document.getElementById("header");
-    // Add a click event listener to h1
-    header.addEventListener("click", function () {
-        alert(":)");
-    });
-});
