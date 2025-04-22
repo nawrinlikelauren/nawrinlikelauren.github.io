b3d491955383c9d712af868ac76475b36bec238e
nawrinlikelauren <107654341+nawrinlikelauren@users.noreply.github.com>
2025-04-22 17:52:29 -0400

    Profile Picture Update

--- animations.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const logo = document.getElementById("logo");
-  // Add a click event listener to h1
-  logo.addEventListener("click", function () {
-    alert(" hi ");
-  });
-});
-
-
