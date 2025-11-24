9c67c248e8b0957f29ab5745dd2980b301d62348
nawrinlikelauren <107654341+nawrinlikelauren@users.noreply.github.com>
2025-11-24 00:09:28 -0500

    Organized files into folders

--- js/animations.js
+++
@@ -1,9 +0,0 @@
-  document.addEventListener("DOMContentLoaded", function () {
-    const backgroundPicture = document.querySelector('.background-picture');  
-    
-    function popBackground() {
-      backgroundPicture.style.transform = 'scale(1)';  
-    }
-    
-    setTimeout(popBackground);
-  });
